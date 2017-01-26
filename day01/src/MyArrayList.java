40a964cf7430b3329def24232e585bef01083b94
cmoser96 <cm898946@gmail.com>
2017-01-25 23:05:38 -0500

    Finished day1

--- day01/src/MyArrayList.java
+++ day01/src/MyArrayList.java
@@ -3,33 +3,63 @@ public class MyArrayList {
 	private int size;
 
 	public MyArrayList() {
-		// TODO
+		elems = new Cow[10];
+		size = 0;
 	}
 
 	public MyArrayList(int capacity) {
-		// TODO
+		elems = new Cow[capacity];
+		size = 0;
 	}
 
 	public void add(Cow c) {
-		// TODO
+		if(size >= elems.length){
+			Cow[] larger = new Cow[elems.length*2];
+			System.arraycopy(elems, 0, larger, 0, elems.length);
+			elems = larger;
+		}
+		elems[size] = c;
+		size++;
 	}
 
 	public int size() {
-		// TODO
-		return -1;
+		return size;
 	}
 
 	public Cow get(int index) {
-		// TODO
-		return null;
+		if(index < 0 || index > size){
+			throw new IndexOutOfBoundsException();
+		}
+		return elems[index];
 	}
 
 	public Cow remove(int index) {
-		// TODO
-		return null;
+		Cow c = get(index);
+		for(int i = index; i < size-1; i++){
+			elems[i] = elems[i+1];
+		}
+		if(size < elems.length/4 && elems.length > 5) {
+			Cow[] larger = new Cow[elems.length/2];
+			System.arraycopy(elems, 0, larger, 0, elems.length/2);
+			elems = larger;
+		}
+		size--;
+		return c;
 	}
 
 	public void add(int index, Cow c) {
-		// TODO
+		if(index < 0 || index > elems.length) {
+			throw new IndexOutOfBoundsException();
+		}
+		if(size >= elems.length){
+			Cow[] larger = new Cow[elems.length*2];
+			System.arraycopy(elems, 0, larger, 0, elems.length);
+			elems = larger;
+		}
+		for(int i = size; i >= index; i--){
+			elems[i+1] = elems[i];
+		}
+		elems[index] = c;
+		size++;
 	}
 }
