9b19de7b7611ea24430aefb49b7c828ac7d628fb
cmoser96 <cm898946@gmail.com>
2017-02-08 22:20:10 -0500

    Day04

--- day04/src/Problems.java
+++ day04/src/Problems.java
@@ -1,21 +1,48 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 public class Problems {
 
     public static Map<Integer, Integer> getCountMap(int[] arr) {
-        // TODO: your code here
-        return null;
+        MyLinearMap<Integer, Integer> map = new MyLinearMap();
+        for(int i=0; i<arr.length; i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i], map.get(arr[i])+1);
+            }
+            else{
+                map.put(arr[i], 1);
+            }
+        }
+        return map;
     }
 
     public static List<Integer> removeKDigits(int[] num, int k) {
-        // TODO: your code here
-        return null;
+        LinkedList<Integer> list = new LinkedList<>();
+        int removes = 0;
+        for(int i=0; i<num.length; i++){
+            while(k>removes && list.size()>0 && num[i]<list.getLast()){
+                list.removeLast();
+                removes++;
+            }
+            if(list.size()+k<num.length){
+                list.addLast(num[i]);
+            }
+        }
+        return list;
     }
 
     public static int sumLists(Node<Integer> h1, Node<Integer> h2) {
-        // TODO: your code here
-        return 0;
+        return getVal(h1)+getVal(h2);
     }
-
+    public static int getVal(Node<Integer> h){
+        int val=0;
+        while(h!=null){
+            val*=10;
+            val+=h.data;
+            h = h.next;
+        }
+        return val;
+    }
 }
