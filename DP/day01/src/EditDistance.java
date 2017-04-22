db303029d80f52ce685a5f90425a23ca18a741e4
cmoser96 <cm898946@gmail.com>
2017-04-22 00:20:37 -0400

    Day01

--- DP/day01/src/EditDistance.java
+++ DP/day01/src/EditDistance.java
@@ -1,8 +1,28 @@
+import java.lang.Math;
 public class EditDistance {
 
     public static int minEditDist(String a, String b) {
-        // TODO: Your code here
-        return 0;
+        if(a.hashCode() == b.hashCode())
+            return 0; //good enough
+        return editDist(a, b, a.length(), b.length());
+    }
+
+    private static int editDist(String a, String b, int m, int n){
+        if(m==0){
+            return n;
+        }
+        if(n==0){
+            return m;
+        }
+
+        if(a.charAt(m-1) == b.charAt(n-1)){
+            return editDist(a, b, m-1, n-1);
+        }
+        else{
+            return 1+ Math.min(editDist(a,b,m,n-1),
+                    Math.min(editDist(a,b,m-1,n),
+                            editDist(a,b,m-1,n-1)));
+        }
     }
 
 }
--- DP/day01/src/TextJustification.java
+++ DP/day01/src/TextJustification.java
@@ -1,8 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
 public class TextJustification {
 
     public static List<Integer> justifyText(String[] w, int m) {
-        // TODO: Your code here
-        return null;
+        int N = w.length;
+        int[] DP = new int[N+1];
+        DP[N] = 0;
+        int[] breaks = new int[N];
+        breaks[N-1] = N;
+
+        for(int i=N-1; i>=0; i--){
+            int end = i+1;
+            int lowest = Integer.MAX_VALUE;
+            for(int j=0; j<=N; j++){
+                int c = cost(w, i, j, m) + DP[j];
+                if(c < lowest){
+                    lowest = c;
+                    end = j;
+                }
+            }
+            DP[i] = lowest;
+            breaks[i] = end;
+        }
+
+        int i = 0;
+        ArrayList<Integer> b = new ArrayList<>();
+        while (i < w.length) {
+            b.add(i);
+            i = breaks[i];
+        }
+        return b;
     }
 
+    private static int cost(String[] w, int low, int high, int m){
+        int length = high-low-1;
+        for(int i=0; i<high; i++){
+            length += w[i].length();
+        }
+        return length>m ? Integer.MAX_VALUE : (int)Math.pow(m-length, 3);
+    }
 }
